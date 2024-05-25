5e2cc6081db7848414392216fb1e5c8ed6b2f4ee
Hanan136 <202106695@stu.uob.edu.bh>
2024-05-25 23:18:56 +0300

    Update Client.py
    op 1

--- Client.py
+++ Client.py
@@ -57,4 +57,10 @@ def fetchSources(client_socket):
         print("4. List all")
         print("5. Back to the main menu")
         option = input("Select an option: ")
+        
+         if option == '1':
+            category = input("Enter category (e.g., business, entertainment, general, health, science, sports, technology): ")
+            params = {'category': category}
+            sources_data = userRequest(client_socket, f'get_news|sources|{json.dumps(params)}')
+            printSources(sources_data)
 
